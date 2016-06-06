c3e87bf4ccb6ee44a61005eed69172c1dbc7c6bf
jacobshiff <jacobshiff@gmail.com>
2016-06-06 11:08:45 -0400

    WIP seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# USERS
+alana = User.create(username: "alana", password: "password")
+jacob = User.create(username: "jacob", password: "password")
+steven = User.create(username: "steven", password: "password")
+
+# CHATROOMS
+chatroom1 = User.create(username: "alana", password: "password")
+chatroom2 = User.create(username: "jacob", password: "password")
+chatroom3 = User.create(username: "steven", password: "password")
+
+
+
