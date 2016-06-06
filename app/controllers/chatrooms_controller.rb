1b9f5c17b2caaff6368093c480c0660ea85e4e15
alanasim <alana.simoncelli@gmail.com>
2016-06-06 10:05:41 -0400

    basic routes and authentication

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,7 @@
 class ChatroomsController < ApplicationController
+  def index
+  end
+
   def show
     @chatroom = Chatroom.find_by(id: params[:id])
     @message = Message.new
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :user_chatrooms
   has_many :chatrooms, through: :user_chatrooms
   has_many :messages
+
+  has_secure_password
+
+  def self.authenticate(username, password)
+    user = User.find_by(username: username)
+    !!user && !!user.authenticate(password)
+  end
 end
