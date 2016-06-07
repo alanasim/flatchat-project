c916c96fd24e4b7854cbf808fb36265be0ad9443
jacobshiff <jacobshiff@gmail.com>
2016-06-07 14:49:36 -0400

    nearly complete phone number verification

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -4,6 +4,12 @@ class PhoneNumbersController < ApplicationController
   end
 
   def create
+    @phone_number = PhoneNumber.find_or_create_by(phone_number:params[:phone_number][:phone_number])
+    @phone_number.generate_pin
+    @phone_number.send_in
+    respond_to do |format|
+    format.js # render app/views/phone_numbers/create.js.erb
+  end
   end
 
   def verify
--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,4 +1,22 @@
 class PhoneNumber < ApplicationRecord
   belongs_to :user
   # validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+
+  def generate_pin
+    self.pin = rand(0000..9999).to_s.rjust(4, "0")
+    save
+  end
+
+  def twilio_client
+    Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+  end
+
+  def send_pin
+    twilio_client.messages.create(
+      to: phone_number,
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      body: "Your PIN is #{pin}"
+    )
+  end
+
 end
