a483d79f5e2910bbafd5515182b4caeed5779349
jacobshiff <jacobshiff@gmail.com>
2016-06-07 16:44:43 -0400

    Working

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -8,15 +8,21 @@ class PhoneNumber < ApplicationRecord
   end
 
   def twilio_client
-    Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+    Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
   end
 
   def send_pin
-    twilio_client.messages.create(
-      to: phone_number,
-      from: ENV['TWILIO_PHONE_NUMBER'],
-      body: "Your PIN is #{pin}"
-    )
+    twilio_client.messages.create(
+    to: phone_number,
+    from: ENV['TWILIO_PHONE_NUMBER'],
+    body: "Your PIN is #{pin}"
+    )
+  end
+
+  def verify(entered_pin)
+    update(verified: true) if self.pin == entered_pin
   end
 
 end
+
+# test.messages.create(to: '2153845679',from: '2038262966',body: "Your PIN is 1234")
