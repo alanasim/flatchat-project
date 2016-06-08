c0e8368d84c1a258e0733ed8597cb8a916d82240
alanasim <alana.simoncelli@gmail.com>
2016-06-08 01:05:54 -0400

    merge issue fixed, working. created message views

---
+++ app/controllers/message_views_controller.rb
@@ -0,0 +1,3 @@
+class MessageViewsController < ApplicationController
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606190159) do
+ActiveRecord::Schema.define(version: 20160608013426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chats", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "message_views", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "message_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
